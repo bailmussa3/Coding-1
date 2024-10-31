dfe7d077af64ecb95af5bc87653f2912f7c29f2b
bailmussa3 <mbaikro869@boyslatin.net>
2024-10-31 12:47:08 +0000

    Learning python assigemnt

---
+++ Unit3_python/pythonFoundation.py
@@ -0,0 +1,34 @@
+# data types = are the fundamental building 
+# blocks of python
+
+# integers = whole numbers
+# strings = any characters wrapped in quotes
+# boolean = true and false 
+# we output data using the print function 
+print(200 * 8)
+print('welcome to python. !@#')
+print(true)
+
+() #round brackets= function
+{} #curly brackets = objects
+[] #square brackets = lists
+
+# varibles = container data
+# we write varibles by first declaring/ giving it a name 
+# we use the equal sign.
+# we then give it data.
+name ='write some text'
+message ='good morning'
+
+# concatenation = combining strings
+print(message, name)
+
+story =" I will be going to"
+location='trick or treating'
+time='at 8pm'
+print(story, location, time)
+
+ValA=10
+valB=20
+
+print(ValA + ValB)
