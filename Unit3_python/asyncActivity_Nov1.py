9528c73a7a61893f0ff18d145746532708a01d52
bailmussa3 <mbaikro869@boyslatin.net>
2024-11-18 13:26:16 +0000

    aSYNC ASSIGEMNT COMPLETED

---
+++ Unit3_python/asyncActivity_Nov1.py
@@ -0,0 +1,54 @@
+# keywords
+# print() - round brackets
+# variables 
+# data types 
+
+# clues 
+# add two numbers to creater 245.
+# variables--> put data inside of the variables--> print results 
+
+a= 105
+b= 140
+
+print(a+b)
+
+
+# if i was ask to create a calendar app 
+#  Days of the week would be best represented by a list of strings.
+# Example: ("monday", "tuesday", "wednesday")
+# The date would be represented using a date object.
+# Example: 1 (for the 1st day of the month) or using a datetime object.
+
+ # What is the diffence  between the float and integer data types?
+ # An integer (int) is a whole number ( 1, -5, 42) without a decimal point.
+# A float is a number that includes a decimal point (3.14, -0.001, 100.0).
+
+# Identify the correct data type for each scenaro?
+# A comment section on a picture on Instagram: String, since it will be text-based.
+comment_section = "Great picture!"
+
+# A survey form asking if a user has ever traveled to another country: Boolean, since it's a yes/no question.
+traveled = True  # or False
+
+# Describe the difference between var, let, and const?
+# // var is function-scoped or globally-scoped and can be re-declared and updated.
+# // let is block-scoped and can be updated but not re-declared in the same block.
+# // const is block-scoped and cannot be updated or re-declared after its initial assignment.
+
+# Create three variables that when multiplied together result in 4000:
+#// Creating three variables
+# let num1 = 10;
+# let num2 = 20;
+# let num3 = 20;
+
+# Describe a scenario where you would use the let variable and when you would use a const variable in a computer program:
+
+# // Use let when you expect to change the value of a variable during the program's execution.
+# // Example: let userAge = 25; userAge = 26;
+const shouldShowWelcomeMessage = true; 
+
+# If there were three things you feel could improve the class, what would those things be and why?
+# // This answer can be a string.
+const improvements = "More coding exercises";
+
+
---
+++ Unit3_python/operatorReview.py
@@ -0,0 +1,22 @@
+ # assigemnt operators
+itemPrice = 0.0 
+
+jeans= 200
+ # artithmetic opertors -  math processes, addition, subtraction, multiplication,
+sendMony= 25.00
+depositAmount= 0.0
+withdrawAmount= 0.0 
+ checkingAccountBalance = 0.0
+ savingAccountBalance = 0.0 
+ iinvestmentAccount= 0.0 
+ print(depositAmount + savingsAccountBalance) 
+
+ # travcker runner app arithemtic operator exampes 
+ firstRunTime= 20.00
+ secondRunTime= 18.00
+ runningDifference = firstRUnTime- secondRunTime
+
+
+ # logical operators
+
+ # comparison operators
