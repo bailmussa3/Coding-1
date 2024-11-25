3a256a1ba6f9d47e921ac3ad4d11a4d1a74c7980
bailmussa3 <mbaikro869@boyslatin.net>
2024-11-25 13:22:55 +0000

    Async assigment redo completed

---
+++ Unit3_python/asyncActivityNov15.py
@@ -0,0 +1,100 @@
+Question 1
+
+Python example 
+
+# Variables representing the correct name and passcode
+name = "MussaBaikro"
+passcode = "24123"
+
+# Input values (example)
+input_name = "MussaBaikro"
+input_passcode = "24123"
+
+# Check if both the name and passcode match
+if input_name == name and input_passcode == passcode:
+    print("Access granted")
+else:
+    print("Access denied")
+
+JavaScript Example
+
+    // Variables representing the correct name and passcode
+let name = "Mussabaikro";
+let passcode = "24123";
+
+// Input values (example)
+let inputName = "Mussabaikro";
+let inputPasscode = "24123";
+
+// Check if both the name and passcode match
+if (inputName === name && inputPasscode === passcode) {
+    console.log("Access granted");
+} else {
+    console.log("Access denied");
+}
+
+Question 2 
+
+python example
+ecg_needed, ecg_received = 10, 4
+oxygen_needed, oxygen_received = 6, 9
+
+if ecg_received < ecg_needed: print("Get more ECG.")
+elif ecg_received > ecg_needed: print("Send extra ECG.")
+else: print("ECG is right.")
+
+if oxygen_received < oxygen_needed: print("Get more Oxygen.")
+elif oxygen_received > oxygen_needed: print("Send extra Oxygen.")
+else: print("Oxygen is right.")
+
+javascript
+
+let ecgNeeded = 10, ecgReceived = 4;
+let oxygenNeeded = 6, oxygenReceived = 9;
+
+if (ecgReceived < ecgNeeded) console.log("Get more ECG.");
+else if (ecgReceived > ecgNeeded) console.log("Send extra ECG.");
+else console.log("ECG is right.");
+
+if (oxygenReceived < oxygenNeeded) console.log("Get more Oxygen.");
+else if (oxygenReceived > oxygenNeeded) console.log("Send extra Oxygen.");
+else console.log("Oxygen is right.");
+
+qUESTION 3
+
+Python Example:
+
+
+#  user data
+message = input("iAM MUSSA ")
+recipient = input("mUSSA: ")
+
+# Send message (print to sending)
+print(f"Sending message to {recipient}: {message}")
+JavaScript Example:
+
+// Capture user data
+let message = prompt(" i AM MUSSA");
+let recipient = prompt("mUSSA");
+
+// Send message (print to simulate sending)
+console.log("Sending message to ${recipient}: ${message}");
+
+Python Example:
+
+# Get user's name and age
+name = input(" MUSSA bAIKRO ")
+age = input(" 15 ")
+
+# Concatenate the string and print
+print("My name is " + MUSSA + ". I am " + 15 + " years old.")
+
+QUESTION 5 
+SYMBLOS
+(Assignment operator): Used to assign a value to a variable.
+
+Example: let x = 5; assigns the value 5 to the variable x. 
+
+(OPERATOR) Returns true if both operands are true. If any operand is false, it returns false.
+Example: true && true returns true, but true && false returns false.
+
